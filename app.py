f566d4da8d5b5864b49543a5640e7ce97d506681
jude-san <judekhay@gmail.com>
2024-01-26 21:52:24 +0000

    new

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ unix_timestamp = int(time.time())
 def process_input(u):
     return [int(x.strip()) for x in u]
 
+# created a variable named `data`` for already existing `data` in `analyze_list_elements` function
+data = []
 
 
 def analyze_list_elements(input_list):
@@ -22,18 +24,29 @@ def analyze_list_elements(input_list):
         data.append(element)
 
     sum_of_elements = sum(input_list)
-    data.remove(element)
+    # data.remove(element)
+    print(f'this data {data}')
     print(f"The sum of all elements in the list is: {sum_of_elements}")
+    # return statements included
+    return sum_of_elements
 
+"""
+The bugs:
+1. In `analyze_list_elements` function which had a code that remove the elements after appending and a return statement missing.
+2. Also `data` variable was not defined
+"""
 
 
 
 def main():
     user_input_list = input("Enter a list of numbers separated by commas: ").split(',')
+    # this changes user input from string to input and seprate them
     my_list = process_input(user_input_list)
+    print(my_list)
 
     
     total = analyze_list_elements(my_list)
+    # print(f'this total: {total}')
 
     if total == sum(data):
 
